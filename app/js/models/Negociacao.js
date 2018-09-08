b174dabda63af5b08f7be653d2483a112e7a5697
felipe fanucchi <felipefanucchi2010@hotmail.com>
2018-09-08 00:22:32 -0300

    readonly adicionado a Negociacao model.

--- app/js/models/Negociacao.js
+++ app/js/models/Negociacao.js
@@ -6,35 +6,14 @@ System.register([], function (exports_1, context_1) {
         setters: [],
         execute: function () {
             Negociacao = (function () {
-                function Negociacao(_data, _quantidade, _valor) {
-                    this._data = _data;
-                    this._quantidade = _quantidade;
-                    this._valor = _valor;
+                function Negociacao(data, quantidade, valor) {
+                    this.data = data;
+                    this.quantidade = quantidade;
+                    this.valor = valor;
                 }
-                Object.defineProperty(Negociacao.prototype, "data", {
-                    get: function () {
-                        return this._data;
-                    },
-                    enumerable: true,
-                    configurable: true
-                });
-                Object.defineProperty(Negociacao.prototype, "quantidade", {
-                    get: function () {
-                        return this._quantidade;
-                    },
-                    enumerable: true,
-                    configurable: true
-                });
-                Object.defineProperty(Negociacao.prototype, "valor", {
-                    get: function () {
-                        return this._valor;
-                    },
-                    enumerable: true,
-                    configurable: true
-                });
                 Object.defineProperty(Negociacao.prototype, "volume", {
                     get: function () {
-                        return this._quantidade * this._valor;
+                        return this.quantidade * this.valor;
                     },
                     enumerable: true,
                     configurable: true
